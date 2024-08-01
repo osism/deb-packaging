59b4732191312a68b4a52ba3cd2e0d29ff382c0b
Christian Berendt <berendt@osism.tech>
2024-08-01 20:41:59 +0200

    Build ovs 3.5 package for aarch64
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ scripts/push-ovs-aarch64.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+
+source /etc/os-release
+
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-osism.harbor.regio.digital}
+DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-packages}
+
+docker push $DOCKER_REGISTRY/$DOCKER_NAMESPACE/ovs-$ID-$VERSION_CODENAME:$OVS_VERSION-aarch64
