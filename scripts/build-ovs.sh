f156b9ac0ecaa6b90cb6dea98ecb554223a95355
Christian Berendt <berendt@osism.tech>
2025-01-23 20:53:08 +0100

    zuul: build ovs packages (#20)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/build-ovs.sh
+++ scripts/build-ovs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-VERSION=v3.3.1
-
 # https://docs.openvswitch.org/en/latest/intro/install/debian/#building-open-vswitch-debian-packages
 
 git clone https://github.com/openvswitch/ovs.git
@@ -9,7 +7,7 @@ git clone https://github.com/openvswitch/ovs.git
 pushd ovs
 git checkout $VERSION
 ./boot.sh
-./configure --with-dpdk=shared
+./configure
 make debian
 dpkg-checkbuilddeps
 make debian-deb
